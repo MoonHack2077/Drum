24915d9767ca6ce5df020067b17d5b54a1a5557d
JuanManuelArenasRincon20077 <arenasrinconj@gmail.com>
2022-02-23 21:15:27 -0500

    Add gitignore and little changes

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    window.addEventListener('keydown',(e)=>{
+
+    
+    //Function for return a random number to become to random color
+    function randomNumber(min,max) {
+        return Math.floor(Math.random()*(max+1-min))+min;
+    }
+
+
+
+    const background = document.querySelector('.background');
+
+    function clip (e){
         const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
         const audio = document.querySelector(`audio[alt-key="${e.keyCode}"]`);
         if(!audio) return
-        // key.forEach(item =>{
-        //     item.classList.add('playing');
-        // })
+
+        const colorOne = randomNumber(4,9);
+        const colorTwo = randomNumber(4,9);
+        const colorThree = randomNumber(4,9);
+        const letters = ['a','b','c','d','e','f']
+        const randomIndex = Math.floor(Math.random()*letters.length);
+
+        key.style.backgroundImage = `radial-gradient(#${colorOne}${letters[randomIndex]}${colorTwo}${letters[randomIndex]}${colorThree}${letters[randomIndex]}, #${colorTwo}${letters[randomIndex]}${colorThree}${letters[randomIndex]}${colorOne}${letters[randomIndex]})`
+
+        background.classList.add('gray');
         key.classList.add('playing');
-        // key.style.opacity = '1';
         audio.currentTime = 0;
         audio.play();
-        window.addEventListener('keyup',()=>{
+    }
+
+    
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => {
+        key.addEventListener('transitionend',()=>{
             key.classList.remove('playing');
-            // key.style.opacity = '0.8';
         })
     })
+    
+    background.addEventListener('transitionend',()=>{
+        background.classList.remove('gray');
+    })
+    
+    window.addEventListener('keydown',clip)
 })
